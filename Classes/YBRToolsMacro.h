309e63d76e5ff39d0406384bcff378d5f5eacc70
WangGuofeng <wangguofeng@live.cn>
2018-06-25 16:18:10 +0800

    0.0.4

--- Classes/YBRToolsMacro.h
+++ Classes/YBRToolsMacro.h
@@ -76,7 +76,7 @@
 #define RGBA(r,g,b,a) [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:a]
 #define RGB(r,g,b) RGBA(r,g,b,1)
 #define RGBHex(colorHex) UIColorFromRGB(colorHex)
-#define RGBAHex(colorHex,alpha) UIColorFromRGBA(colorHex,a)
+#define RGBAHex(colorHex,alpha) UIColorFromRGBA(colorHex,alpha)
 
 #define kColorWithHex(rgbValue) \
 [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16)) / 255.0 \
